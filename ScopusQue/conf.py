8dc4bac7e6f1542bbbdb381665fc763853ff997c
raymond301 <raymond.moore301@gmail.com>
2016-02-16 20:14:09 -0600

    testing wx lib

--- ScopusQue/conf.py
+++ ScopusQue/conf.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# This is
-
-import sys, os
 
 version = '0.1'
 workunits = ["BSI IS1", "BSI IS2", "BSI IS Lead", "Biostats SPA", "Biostats MS"]
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ScopusPubMetricsWebby',
+    version='1',
+    packages=['ScopusQue'],
+    url='',
+    license='',
+    author='raymond301',
+    author_email='',
+    description=''
+)
